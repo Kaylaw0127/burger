b27e9152c5c403f994e5a29f5d5ccc3de0de6e18
Kayla Ward <kaylaw0127@gmail.com>
2020-04-09 13:10:49 -0700

    got burgers to change rows

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,45 +1,44 @@
 $(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("devoured");
-  
-      var devouredState = {
-        devoured: newDevoured
-      };
-  
-      // Sends the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: devouredState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-         
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#bu").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
-      };
-  
-      // Sends the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          
-          location.reload();
-        }
-      );
-    });
-  
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("devoured");
+
+    var devourStatus = {
+      devoured: newDevour
+    };
+
+    
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devourStatus
+    }).then(
+      function() {
+        console.log("changed devour to", newDevour);
+       
+        location.reload();
+      }
+    );
   });
+
+  $(".create-form").on("submit", function(event) {
+    
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#bu").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+   
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+       
+        location.reload();
+      }
+    );
+  });
+});
