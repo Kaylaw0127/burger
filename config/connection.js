755dc7f55507c3a20d9f49bb64dbc79be6c84ca7
Kayla Ward <kaylaw0127@gmail.com>
2020-04-09 12:58:35 -0700

    fixed burger-block

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
+var connection; 
 
-let connection;
-if (process.env.JAWS_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWS_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Daisy1121!",
+    password: "root",
     database: "burgers_db"
   });
-}
+// }
 
 connection.connect(function(err) {
   if (err) {
