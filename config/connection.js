91ed4d30e13852de9396ab2ba0751c514d68ccbc
Kayla Ward <kaylaw0127@gmail.com>
2020-04-23 17:54:33 -0700

    added JAWS db

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 var connection; 
 
-// if (process.env.JAWS_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWS_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,7 +11,7 @@ var connection;
     password: "root",
     database: "burgers_db"
   });
-// }
+}
 
 connection.connect(function(err) {
   if (err) {
